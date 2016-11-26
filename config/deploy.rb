01fb2b1824cacb9797445f99de94baef270f71fe
Dima Nazarchuk <dmitry.nazarchuk@kandasoft.com>
2016-11-26 13:11:02 +0300

    Update deploy configs

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,9 +1,13 @@
 # Change these
-server '138.68.82.47', roles: %w(web app db)
+server '138.68.82.47', roles: %w(web app db), user: 'deployer'
+
+set :user, 'deployer'
+
+set :rvm_type, :user
+set :rvm_ruby_version, '2.3.3'
 
 set :repo_url,        'git@github.com:dimanazarchuk/gopose-api.git'
 set :application,     'gopose'
-set :user,            'deployer'
 set :puma_threads,    [4, 16]
 set :puma_workers,    0
 
@@ -13,7 +17,7 @@ set :puma_workers,    0
 set :stage,           :production
 set :deploy_via,      :remote_cache
 set :deploy_to,       "/home/#{fetch(:user)}/apps/#{fetch(:application)}"
-set :puma_bind,       "unix://#{shared_path}/tmp/sockets/#{fetch(:application)}-puma.sock"
+set :puma_bind,       "unix:/#{shared_path}/tmp/sockets/#{fetch(:application)}-puma.sock"
 set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
 set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
 set :puma_access_log, "#{release_path}/log/puma.error.log"
@@ -23,14 +27,18 @@ set :puma_preload_app, true
 set :puma_worker_timeout, nil
 set :puma_init_active_record, true  # Change to false when not using ActiveRecord
 set :ssh_options, { forward_agent: true }
+set :deploy_via, :copy
 
 ## Defaults:
-set :scm,           :git
-set :branch,        :master
+# set :scm,           :git
+# set :branch,        :master
 # set :format,        :pretty
 # set :log_level,     :debug
 # set :keep_releases, 5
 
+# Default value for :linked_files is []
+set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')
+set :linked_dirs, fetch(:linked_dirs, []).push(*%w(log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system))
 ## Linked Files & Directories (Default None):
 # set :linked_files, %w{config/database.yml}
 # set :linked_dirs,  %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
@@ -75,7 +83,6 @@ namespace :deploy do
   end
 
   before :starting,     :check_revision
-  after  :finishing,    :compile_assets
   after  :finishing,    :cleanup
   after  :finishing,    :restart
 end
